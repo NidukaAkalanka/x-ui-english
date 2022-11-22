1f66fc51b1cdec75bed82868a6be7184fb445cb7
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2022-11-22 07:37:04 +0530

    Update stats_notify_job.go

--- web/job/stats_notify_job.go
+++ web/job/stats_notify_job.go
@@ -2,14 +2,16 @@ package job
 
 import (
 	"fmt"
-	"net"
 	"os"
+	"os/exec"
+	"strconv"
 	"time"
 	"x-ui/logger"
 	"x-ui/util/common"
 	"x-ui/web/service"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+var SSHLoginUser int
 
 type LoginStatus byte
 
@@ -19,39 +21,15 @@ const (
 )
 
 type StatsNotifyJob struct {
-	enable         bool
-	xrayService    service.XrayService
-	inboundService service.InboundService
-	settingService service.SettingService
+	enable          bool
+	telegramService service.TelegramService
+	xrayService     service.XrayService
+	inboundService  service.InboundService
+	settingService  service.SettingService
 }
 
 func NewStatsNotifyJob() *StatsNotifyJob {
 	return new(StatsNotifyJob)
-}
-
-func (j *StatsNotifyJob) SendMsgToTgbot(msg string) {
-	//Telegram bot basic info
-	tgBottoken, err := j.settingService.GetTgBotToken()
-	if err != nil || tgBottoken == "" {
-		logger.Warning("sendMsgToTgbot failed,GetTgBotToken fail:", err)
-		return
-	}
-	tgBotid, err := j.settingService.GetTgBotChatId()
-	if err != nil {
-		logger.Warning("sendMsgToTgbot failed,GetTgBotChatId fail:", err)
-		return
-	}
-
-	bot, err := tgbotapi.NewBotAPI(tgBottoken)
-	if err != nil {
-		fmt.Println("get tgbot error:", err)
-		return
-	}
-	bot.Debug = true
-	fmt.Printf("Authorized on account %s", bot.Self.UserName)
-	info := tgbotapi.NewMessage(int64(tgBotid), msg)
-	//msg.ReplyToMessageID = int(tgBotid)
-	bot.Send(info)
 }
 
 //Here run is a interface method of Job interface
@@ -60,78 +38,120 @@ func (j *StatsNotifyJob) Run() {
 		return
 	}
 	var info string
-	//get hostname
-	name, err := os.Hostname()
-	if err != nil {
-		fmt.Println("get hostname error:", err)
-		return
-	}
-	info = fmt.Sprintf("Hostname:%s\r\n", name)
-	//get ip address
-	var ip string
-	netInterfaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Println("net.Interfaces failed, err:", err.Error())
-		return
-	}
-
-	for i := 0; i < len(netInterfaces); i++ {
-		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
-			addrs, _ := netInterfaces[i].Addrs()
-
-			for _, address := range addrs {
-				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						ip = ipnet.IP.String()
-						break
-					} else {
-						ip = ipnet.IP.String()
-						break
-					}
-				}
-			}
-		}
-	}
-	info += fmt.Sprintf("IP:%s\r\n \r\n", ip)
-
-	//get traffic
-	inbouds, err := j.inboundService.GetAllInbounds()
-	if err != nil {
-		logger.Warning("StatsNotifyJob run failed:", err)
-		return
-	}
-	//NOTE:If there no any sessions here,need to notify here
-	//TODO:分节点推送,自动转化格式
-	for _, inbound := range inbouds {
-		info += fmt.Sprintf("Node name:%s\r\nPort:%d\r\nUpload↑:%s\r\nDownload↓:%s\r\nTotal:%s\r\n", inbound.Remark, inbound.Port, common.FormatTraffic(inbound.Up), common.FormatTraffic(inbound.Down), common.FormatTraffic((inbound.Up + inbound.Down)))
-		if inbound.ExpiryTime == 0 {
-			info += fmt.Sprintf("Expire date:unlimited\r\n \r\n")
-		} else {
-			info += fmt.Sprintf("Expire date:%s\r\n \r\n", time.Unix((inbound.ExpiryTime/1000), 0).Format("2006-01-02 15:04:05"))
-		}
-	}
-	j.SendMsgToTgbot(info)
+	info = j.GetsystemStatus()
+	j.telegramService.SendMsgToTgbot(info)
 }
 
 func (j *StatsNotifyJob) UserLoginNotify(username string, ip string, time string, status LoginStatus) {
 	if username == "" || ip == "" || time == "" {
-		logger.Warning("UserLoginNotify failed,invalid info")
+		logger.Warning("UserLoginNotify failed, invalid info")
 		return
 	}
 	var msg string
 	//get hostname
 	name, err := os.Hostname()
 	if err != nil {
-		fmt.Println("get hostname error:", err)
+		fmt.Println("get hostname error: ", err)
 		return
 	}
 	if status == LoginSuccess {
-		msg = fmt.Sprintf("Successfully logged-in to the panel\r\nHostname:%s\r\n", name)
+		msg = fmt.Sprintf("Panel login successful reminder\r\nHost name: %s\r\n", name)
 	} else if status == LoginFail {
-		msg = fmt.Sprintf("Login to the panel was unsuccessful\r\nHostname:%s\r\n", name)
+		msg = fmt.Sprintf("Panel login failure reminder reminder\r\nHost name: %s\r\n", name)
 	}
-	msg += fmt.Sprintf("Time:%s\r\n", time)
-	msg += fmt.Sprintf("Username:%s\r\n", username)
-	msg += fmt.Sprintf("IP:%s\r\n", ip)
-	j.SendMsgToTgbot(msg)
+	msg += fmt.Sprintf("Time: %s\r\n", time)
+	msg += fmt.Sprintf("User: %s\r\n", username)
+	msg += fmt.Sprintf("IP: %s\r\n", ip)
+	j.telegramService.SendMsgToTgbot(msg)
+}
+
+func (j *StatsNotifyJob) SSHStatusLoginNotify(xuiStartTime string) {
+	getSSHUserNumber, error := exec.Command("bash", "-c", "who | awk  '{print $1}' | wc -l").Output()
+	if error != nil {
+		fmt.Println("getSSHUserNumber error: ", error)
+		return
+	}
+	var numberInt int
+	numberInt, error = strconv.Atoi(common.ByteToString(getSSHUserNumber))
+	if error != nil {
+		return
+	}
+	if numberInt > SSHLoginUser {
+		var SSHLoginInfo string
+		SSHLoginUser = numberInt
+		//hostname
+		name, err := os.Hostname()
+		if err != nil {
+			fmt.Println("get hostname error: ", err)
+			return
+		}
+		//Time compare,need if x-ui got restart while ssh already exist users
+		SSHLoginTime, error := exec.Command("bash", "-c", "who | awk  '{print $3,$4}' | tail -n 1 ").Output()
+		if error != nil {
+			fmt.Println("getLoginTime error: ", error.Error())
+			return
+		}
+		var SSHLoginTimeStr string
+		SSHLoginTimeStr = common.ByteToString(SSHLoginTime)
+		t1, err := time.Parse("2006-01-02 15:04:05", SSHLoginTimeStr)
+		t2, err := time.Parse("2006-01-02 15:04:05", xuiStartTime)
+		if t1.Before(t2) || err != nil {
+			fmt.Printf("SSHLogin[%s] early than XUI start[%s]\r\n", SSHLoginTimeStr, xuiStartTime)
+		}
+
+		SSHLoginUserName, error := exec.Command("bash", "-c", "who | awk  '{print $1}'| tail -n 1").Output()
+		if error != nil {
+			fmt.Println("getSSHLoginUserName error: ", error.Error())
+			return
+		}
+
+		SSHLoginIpAddr, error := exec.Command("bash", "-c", "who | tail -n 1 | awk -F [\\(\\)] 'NR==1 {print $2}'").Output()
+		if error != nil {
+			fmt.Println("getSSHLoginIpAddr error: ", error)
+			return
+		}
+
+		SSHLoginInfo = fmt.Sprintf("⚠ SSH user login reminder:\r\n")
+		SSHLoginInfo += fmt.Sprintf("Host name: %s\r\n", name)
+		SSHLoginInfo += fmt.Sprintf("Login user: %s", SSHLoginUserName)
+		SSHLoginInfo += fmt.Sprintf("Log in time: %s", SSHLoginTime)
+		SSHLoginInfo += fmt.Sprintf("Login IP: %s", SSHLoginIpAddr)
+		SSHLoginInfo += fmt.Sprintf("Currently logging in the number of users: %s", getSSHUserNumber)
+		j.telegramService.SendMsgToTgbot(SSHLoginInfo)
+	} else {
+		SSHLoginUser = numberInt
+	}
+}
+
+func (j *StatsNotifyJob) GetsystemStatus() string {
+	var info string
+	//get hostname
+	name, err := os.Hostname()
+	if err != nil {
+		fmt.Println("get hostname error:", err)
+		return ""
+	}
+	info = fmt.Sprintf("Host name: %s\r\n", name)
+	//get ip address
+	var ip string
+	ip = common.GetMyIpAddr()
+	info += fmt.Sprintf("IP address: %s\r\n \r\n", ip)
+
+	//get traffic
+	inbouds, err := j.inboundService.GetAllInbounds()
+	if err != nil {
+		logger.Warning("StatsNotifyJob run failed: ", err)
+		return ""
+	}
+	//NOTE:If there no any sessions here,need to notify here
+	//TODO:Sub -node push, automatic conversion format
+	for _, inbound := range inbouds {
+		info += fmt.Sprintf("Node name: %s\r\n Port: %d\r\n Uplink traffic↑: %s\r\n Downlink traffic↓: %s\r\n Total traffic: %s\r\n", inbound.Remark, inbound.Port, common.FormatTraffic(inbound.Up), common.FormatTraffic(inbound.Down), common.FormatTraffic((inbound.Up + inbound.Down)))
+		if inbound.ExpiryTime == 0 {
+			info += fmt.Sprintf("Understanding time: indefinitely\r\n \r\n")
+		} else {
+			info += fmt.Sprintf("Expire date: %s\r\n \r\n", time.Unix((inbound.ExpiryTime/1000), 0).Format("2006-01-02 15:04:05"))
+		}
+	}
+	return info
 }
