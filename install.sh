d9fdfc966816e9316a2d8300aeea231722e83a62
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2022-11-23 10:08:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -197,17 +197,16 @@ install_xui() {
     
     install_base
     download_xui $1
-     
+    
+    systemctl daemon-reload
+    systemctl enable x-ui >/dev/null 2>&1
+    systemctl start x-ui
+    
     cd
     rm /etc/x-ui-english/x-ui-english.db -rf # Deleting empty new db
     mv /etc/x-ui-english.db.bak /etc/x-ui-english/x-ui-english.db # Bringing the backed up db
     
     panel_config
-    
-
-    systemctl daemon-reload
-    systemctl enable x-ui >/dev/null 2>&1
-    systemctl start x-ui
     systemctl restart x-ui
     
     cd $cur_dir
